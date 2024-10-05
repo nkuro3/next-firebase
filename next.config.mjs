5338a7de8e78a21689c2388a7a3a5b68f21cca7c
nkuro <63081563+nkuro3@users.noreply.github.com>
2024-10-05 13:23:41 +0900

    prepare repository

---
+++ next.config.mjs
@@ -0,0 +1,40 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY"
+          },
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, "")
+          }
+        ]
+      }
+    ];
+  }
+};
+
+export default nextConfig;
+
+const cspHeader = `
+default-src 'self' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
+script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com https://www.google-analytics.com;
+style-src 'self' 'unsafe-inline' https://www.gstatic.com;
+img-src 'self' blob: data: https://www.googletagmanager.com https://www.gstatic.com;
+font-src 'self' https://www.gstatic.com;
+object-src 'none';
+base-uri 'self';
+form-action 'self';
+frame-ancestors 'none';
+frame-src 'self' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
+child-src 'self' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
+connect-src 'self' https://www.google.com https://www.gstatic.com https://www.google-analytics.com;
+upgrade-insecure-requests;
+`;
