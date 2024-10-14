ea610d541a8d868ca40bd0254d957a8fa617ad7f
nkuro <63081563+nkuro3@users.noreply.github.com>
2024-10-15 08:34:50 +0900

    Update CSP

--- next.config.mjs
+++ next.config.mjs
@@ -26,17 +26,15 @@ export default nextConfig;
 const cspHeader = `
 default-src 'self' https://*.firebaseapp.com https://*.googleapis.com;
 script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googletagmanager.com https://*.google.com https://*.gstatic.com https://*.google-analytics.com https://vercel.live https://*.firebaseapp.com https://*.googleapis.com;
-style-src 'self' 'unsafe-inline' https://*.gstatic.com;
+style-src 'self' 'unsafe-inline' https://*.gstatic.com https://vercel.live/fonts;
 img-src 'self' blob: data: https://*.googletagmanager.com https://*.gstatic.com https://*.googleapis.com https://*.firebaseapp.com https://vercel.live http://127.0.0.1:9199;
-font-src 'self' https://*.gstatic.com;
+font-src 'self' https://*.gstatic.com https://vercel.live/fonts;
 object-src 'none';
 base-uri 'self';
 form-action 'self';
 frame-ancestors 'none';
 frame-src 'self' https://*.googletagmanager.com https://*.google.com https://*.gstatic.com https://*.firebaseapp.com https://vercel.live;
 child-src 'self' https://*.googletagmanager.com https://*.google.com https://*.gstatic.com https://*.firebaseapp.com https://vercel.live;
-connect-src 'self' https://*.google.com https://*.gstatic.com https://*.google-analytics.com https://*.firebaseio.com https://*.googleapis.com wss://*.firebaseio.com https://*.firebaseapp.com https://vercel.live http://127.0.0.1:9099 http://127.0.0.1:8080 http://127.0.0.1:9199;
+connect-src 'self' https://*.google.com https://*.gstatic.com https://*.google-analytics.com https://*.firebaseio.com https://*.googleapis.com wss://*.firebaseio.com wss://ws-us3.pusher.com https://sockjs-us3.pusher.com https://*.firebaseapp.com https://vercel.live http://127.0.0.1:9099 http://127.0.0.1:8080 http://127.0.0.1:9199;
 upgrade-insecure-requests;
-`
-  .replace(/\s{2,}/g, " ")
-  .trim();
+`;
