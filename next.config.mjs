af709795f4435e7db72f2d87b5d97c9ba58ec4c4
nkuro <63081563+nkuro3@users.noreply.github.com>
2024-10-15 08:22:11 +0900

    Update CSP

--- next.config.mjs
+++ next.config.mjs
@@ -24,17 +24,19 @@ const nextConfig = {
 export default nextConfig;
 
 const cspHeader = `
-default-src 'self' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
-script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com https://www.google-analytics.com;
-style-src 'self' 'unsafe-inline' https://www.gstatic.com;
-img-src 'self' blob: data: https://www.googletagmanager.com https://www.gstatic.com http://127.0.0.1:9199;
-font-src 'self' https://www.gstatic.com;
+default-src 'self' https://*.firebaseapp.com https://*.googleapis.com;
+script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googletagmanager.com https://*.google.com https://*.gstatic.com https://*.google-analytics.com https://vercel.live https://*.firebaseapp.com https://*.googleapis.com;
+style-src 'self' 'unsafe-inline' https://*.gstatic.com;
+img-src 'self' blob: data: https://*.googletagmanager.com https://*.gstatic.com https://*.googleapis.com https://*.firebaseapp.com http://127.0.0.1:9199;
+font-src 'self' https://*.gstatic.com;
 object-src 'none';
 base-uri 'self';
 form-action 'self';
 frame-ancestors 'none';
-frame-src 'self' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
-child-src 'self' https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
-connect-src 'self' https://www.google.com https://www.gstatic.com https://www.google-analytics.com http://127.0.0.1:9099 http://127.0.0.1:8080 http://127.0.0.1:9199;
+frame-src 'self' https://*.googletagmanager.com https://*.google.com https://*.gstatic.com https://*.firebaseapp.com;
+child-src 'self' https://*.googletagmanager.com https://*.google.com https://*.gstatic.com https://*.firebaseapp.com;
+connect-src 'self' https://*.google.com https://*.gstatic.com https://*.google-analytics.com https://*.firebaseio.com https://*.googleapis.com wss://*.firebaseio.com https://*.firebaseapp.com http://127.0.0.1:9099 http://127.0.0.1:8080 http://127.0.0.1:9199;
 upgrade-insecure-requests;
-`;
+`
+  .replace(/\s{2,}/g, " ")
+  .trim();
